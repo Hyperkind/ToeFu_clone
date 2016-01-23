faeed28b24ea71e72558da011d63853112b81e16
Jon Ah-Nee <jonahnee@gmail.com>
2016-01-23 11:57:28 -1000

    added assets

--- public/js/states/boot.js
+++ public/js/states/boot.js
@@ -8,7 +8,19 @@ ToeFu_Clone.Boot = function() {
 };
 
 ToeFu_Clone.Boot.prototype.preload = function() {
-  // will preload all assets
+//autoload each asset by type
+  Object.keys(ToeFu_Clone.ASSETS).forEach(function(type) {
+    for(var asset in ToeFu_Clone.ASSETS[type]) {
+      ToeFu_Clone.game.load[ type.toLowerCase() ](
+        ToeFu_Clone.ASSETS[type][ asset ].name,
+        ToeFu_Clone.ASSETS[type][ asset ].path,
+        ToeFu_Clone.ASSETS[type][ asset ].width,
+        ToeFu_Clone.ASSETS[type][ asset ].height,
+        ToeFu_Clone.ASSETS[type][ asset ].frames
+      );
+    }
+  });
+
 };
 
 ToeFu_Clone.Boot.prototype.create = function() {
