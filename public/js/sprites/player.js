2569a1e08421f2cede5709574122d3453f58fa92
Jon Ah-Nee <jonahnee@gmail.com>
2016-01-23 12:39:44 -1000

    added animations to player

--- public/js/sprites/player.js
+++ public/js/sprites/player.js
@@ -1,18 +1,36 @@
-// sprite class constructor
-ToeFu_Clone.Player = function (game, id, name) {
-  this.game = game;
-  this.id = id;
-  this.name = name? name : 'Player ' + (id+1);
+(function () {
 
-  // super constructor call
-  Phaser.Sprite.call(this, game, 0, 0, ToeFu_Clone.ASSETS.SPRITESHEET.PLAYER.name);
+  // private static variables
+  var ANIMATIONS = {
+    IDLE : {
+      name : 'idle',
+      frames : [0,1,2,3],
+      fps : 5
+    }
+  };
 
-};
+  // sprite class constructor
+  ToeFu_Clone.Player = function (game, id, name) {
+    this.game = game;
+    this.id = id;
+    this.name = name? name : 'Player ' + (id+1);
 
-// extend Sprite prototype
-ToeFu_Clone.Player.prototype = Object.create(Phaser.Sprite.prototype, {
-  constructor: {
-    value: ToeFu_Clone.Player
-  }
-});
+    // super constructor call
+    Phaser.Sprite.call(this, game, 0, 0, ToeFu_Clone.ASSETS.SPRITESHEET.PLAYER.name);
+
+    // set animations
+    this.animations.add(ANIMATIONS.IDLE.name, ANIMATIONS.IDLE.frames);
+
+    // play the initial animation
+    this.animations.play(ANIMATIONS.IDLE.name, ANIMATIONS.IDLE.fps, true);
+  };
+
+  // extend Sprite prototype
+  ToeFu_Clone.Player.prototype = Object.create(Phaser.Sprite.prototype, {
+    constructor: {
+      value: ToeFu_Clone.Player
+    }
+  });
+
+})();
 
